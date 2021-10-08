d4506375cb978ab2cecfabaf80af046c16afd779
Garyfallos Konstantinoudis <garyfallos.konstantinoudis@ispm.unibe.ch>
2021-10-08 17:27:30 +0100

    Clean deaths and create final file for analysis

---
+++ 5_MortalityData.R
@@ -0,0 +1,9 @@
+
+
+# Created 08.10.2021
+
+
+# Clean mortality data and bring all together to create the final file. 
+
+
+######################################################################################
