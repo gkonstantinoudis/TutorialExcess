8a7c1a88e68b98eed0a6cb438ae9d9b78e32802e
Garyfallos Konstantinoudis <garyfallos.konstantinoudis@ispm.unibe.ch>
2021-12-10 12:22:30 +0000

    adding cross validation and renaming the sequence

--- 4_PopulationInterpolation.R
+++ 4_PopulationInterpolation.R
@@ -16,7 +16,7 @@ installpack <- FALSE
 
 
 if(installpack){
-  install.packages(c("readxl", "dplyr", "tidyr"))
+  install.packages(c("readxl", "dplyr", "tidyr", "ggplot2", "patchwork"))
 }
 
 
@@ -25,11 +25,12 @@ if(installpack){
 library(readxl)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
+library(patchwork)
 
 
 
-
-load("pop15_20_final.RData")
+load("Output/pop15_20_final.RData")
 pop <- pop15_20; rm(pop15_20)
 
 
@@ -83,6 +84,38 @@ pop_weekly <- left_join(pop_weekly, EUROSTAT[,c("EURO_LABEL", "year", "day2pred"
 
 pop_weekly <- left_join(pop_weekly, pop, by = c("year" = "year", "age" = "ageg", "sex" = "sex", "region" = "NUTS318CD"))
 
+
+
+## First two panels of the plot
+pop %>% group_by(NUTS318CD, sex, ageg) %>% 
+  summarise(coef = as.vector(coef(lm(population ~ year)))) %>% 
+  filter(NUTS318CD %in% "VE", sex %in% "female", ageg %in% "40-59") -> coef.VE
+
+pop %>% filter(NUTS318CD %in% "VE", sex %in% "female", ageg %in% "40-59") -> pop.VE
+
+ggplot() + geom_point(data = pop.VE, aes(x=year, y=population)) + 
+  geom_point(aes(x=2021, y = coef.VE$coef %*% c(1, 2021)), col = "red") + 
+  geom_line(aes(x=2015:2021, y = c(coef.VE$coef[1] + coef.VE$coef[2]*2015:2021)), linetype = 2, col = "red") + 
+  scale_x_continuous(breaks = 2015:2021) + ylim(c(136400, 139200)) +
+  theme_light() +
+  ggtitle("A.") + 
+  theme(text = element_text(size = 8), 
+        plot.margin=unit(c(0,0,0,0), "cm")) -> p1
+
+
+ggplot() + geom_point(data = pop.VE, aes(x=year, y=population)) + 
+  geom_point(aes(x=2021, y = coef.VE$coef %*% c(1, 2021)), col = "red") + 
+  geom_line(aes(x=2015:2021, y = c(pop.VE$population, coef.VE$coef %*% c(1, 2021))), linetype = 1, col = "black") + 
+  scale_x_continuous(breaks = 2015:2021) + ylim(c(136400, 139200)) + 
+  theme_light() + geom_rect(aes(xmin = 2014.9, xmax = 2016.1, ymin = 138300, ymax = 139000),
+                            fill = "transparent", color = "blue", size = 0.5) + 
+  ggtitle("B.") + 
+  theme(text = element_text(size = 8), 
+        plot.margin=unit(c(0,0,0,0), "cm"))-> p2
+##--##
+
+
+
 # we do not have the population for 2021, we can calculate with linear interpolation
 pop %>% group_by(NUTS318CD, sex, ageg) %>% 
   summarise(population = as.vector(coef(lm(population ~ year)) %*% c(1, 2021))) %>% 
@@ -119,6 +152,28 @@ colnames(pop_weekly)[6] <- "population"
 saveRDS(pop_weekly, file = "Output/pop_weekly")
 
 
+# For the plot we are focusing on Veneto and to female population less than 40
+
+# third panel of the plot
+
+dat_weekly_VE <- pop_weekly %>% filter(NUTS318CD  %in% "VE", sex %in% "female", age %in% "40-59", year == 2015) %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL)))
+
+ggplot() + geom_point(data = dat_weekly_VE, aes(x=x, y=population)) + 
+  scale_x_continuous(breaks = dat_weekly_VE$x, labels = dat_weekly_VE$EURO_LABEL, expand = c(0.02, 0.02)) + 
+  theme_light() + ylim(c(138300, 139000)) + 
+  theme(axis.text.x=element_text(angle = -90, hjust = 1, vjust = 0.2)) + 
+  geom_rect(aes(xmin = 0.6, xmax = 53.4, ymin = 138300, ymax = 139000), fill = "transparent", color = "blue", size = 0.5) + 
+  ggtitle("C.") + xlab("Estimated weekly population during 2015-2016") + 
+  theme(text = element_text(size = 8), 
+        plot.margin=unit(c(0,0,0,0), "cm")) -> p3
+   
+  
+# Bring plots together
+
+png("PopulationPlot.png", width = 17, height = 14, units = "cm", res = 300)
+(p1|p2)/p3
+dev.off()
 
 
 
---
+++ 6_CrossValidation.R
@@ -0,0 +1,192 @@
+
+
+# Created 11.10.2021
+
+# Cross validation
+
+#-----------------------------------------------------------------
+
+
+
+setwd("E:/Postdoc Imperial/Projects/COVID19 Greece/TutorialExcessOutput/")
+path2save <- "Output/"
+
+installpack <- FALSE
+
+
+if(installpack){
+  install.packages(c("sf", "dplyr", "spdep"))
+  install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+}
+
+
+
+library(INLA)
+library(dplyr)
+library(sf)
+library(spdep)
+
+
+
+
+################################################################################
+# Cross validation functions
+################################################################################
+
+# function to fit INLA and store the cv
+inla.cv <- function(Y){
+  
+  in.mod <- 
+    inla(formula = as.formula(Y),
+         data=datCV,
+         family="Poisson",  
+         verbose = FALSE, 
+         control.family=control.family,
+         control.compute=list(config = TRUE), 
+         control.mode=list(restart=TRUE),
+         num.threads = round(parallel::detectCores()*.8), 
+         control.predictor = list(link = 1)
+    )
+  
+  
+  
+  post.samples <- inla.posterior.sample(n = 1000, result = in.mod)
+  
+  # store the results
+  list.CV.results <- list(
+    prediction = lapply(post.samples, function(X) exp(X$latent[testIndexes])),
+    true_values =  datCV_firslop$deaths[testIndexes]
+  )
+  
+  return(list.CV.results)
+  
+}
+
+
+
+# Function for computing correlation & coverage probability
+postpredchecks <- function(Y){
+  
+  pred.samples <- lapply(Y, function(X) do.call(cbind, X$prediction))
+  true_values <- do.call(c, lapply(Y, function(X) X$true_values))
+  
+  # samples from a Poisson
+  set.seed(11)
+  pois.samples <- lapply(pred.samples,
+                         function(Z) apply(Z, 2, function(X) rpois(n = length(X), lambda = X)))
+  
+  # calculate quantiles
+  pois.quant <- as.data.frame(do.call(rbind, lapply(pois.samples, 
+                                                    function(X) t(apply(X, 1, function(Z) quantile(Z, probs = c(0.025, 0.975)))))))
+  cov.prob = mean((pois.quant$`2.5%` <= true_values) & (pois.quant$`97.5%` > true_values))
+  
+  
+  res <- list(
+    correl = apply(do.call(rbind, pois.samples), 2, function(X) cor(X, true_values)), 
+    cov.prob = mean((pois.quant$`2.5%` <= true_values) & (pois.quant$`97.5%` > true_values)) # if you exclude 0s is very low
+  )
+  
+  return(res)
+  
+}
+
+################################################################################
+################################################################################
+
+
+
+# define the groups
+groups4cv = as.data.frame(expand.grid(age.group = c("less40", "40-59", "60-69", "70-79", "80plus"),
+                                      sex = c("F", "M")))
+
+
+finaldb = readRDS("Output/findata")
+
+shp = read_sf("ProvCM01012020_g_WGS84.shp")
+shp = shp[order(shp$COD_PROV),]
+shp$COD_RIP <- as.numeric(as.factor(shp$COD_PROV))
+colnames(shp)[1] <- "id.space"
+
+W.nb <- poly2nb(shp)
+nb2INLA("W.adj", W.nb) 
+
+
+# set up formula and priors
+
+formula = 
+  deaths ~ 1 + offset(log(population)) + hol + 
+  f(id.tmp, model='rw2', hyper=hyper.iid, constr = TRUE, scale.model = TRUE) +
+  f(id.year, model='iid', hyper=hyper.iid, constr = TRUE) + 
+  f(id.time, model='rw1', hyper=hyper.iid, constr = TRUE, scale.model = TRUE, cyclic = TRUE) +
+  f(id.space, model='bym2', graph="W.adj", scale.model = TRUE, constr = TRUE, hyper = hyper.bym)
+
+# priors
+hyper.bym <- list(theta1 = list('PCprior', c(1, 0.01)), theta2 = list('PCprior', c(0.5, 0.5)))
+hyper.iid <- list(theta = list(prior="pc.prec", param=c(1, 0.01)))
+
+# Under Poisson uses default set up
+control.family=inla.set.control.family.default()
+
+
+
+# Perform space-time cross validation
+list.results <- list()
+years <- 2015:2019
+data = finaldb 
+
+t_0 <- Sys.time()
+
+for(j in 1:nrow(groups4cv)){
+  
+  print(j)
+  
+  agegroup <- as.character(groups4cv$age.group[j])
+  sexg <- as.character(groups4cv$sex[j])
+  
+  # recreate indexes
+  data$id.space <- as.numeric(as.factor(data$PROV))
+  data$id.time <- as.numeric(substr(data$EURO_LABEL, start = 7, stop = 8))
+  data$id.tmp <- inla.group(data$mean.temp, n = 100, method = "cut", idx.only = TRUE)
+  data$id.year <- data$year - 2014
+  data <- data[order(data$id.space),]
+  
+  data %>% filter((ageg %in% agegroup) & (sex %in% sexg)) -> datCV_firslop
+  
+  list.CV.results.spacetime <- list()
+  
+  for(i in 1:5){
+    
+    datCV <- datCV_firslop
+    testIndexes <- which(datCV$year %in% years[i], arr.ind=TRUE)
+    datCV$deaths[testIndexes] <- NA
+    
+    list.CV.results.spacetime[[i]] <- inla.cv(Y = formula)
+    gc()
+    
+  }
+  
+  saveRDS(list.CV.results.spacetime,
+          file = paste0(path2save, "yearCV_", agegroup, "_", sexg))
+  
+}
+
+t_1 <- Sys.time()
+print(t_1 - t_0)
+
+
+
+
+
+
+
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+
+
+
+
+
+
+
