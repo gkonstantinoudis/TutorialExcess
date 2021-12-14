48c9f70ec7221eb3627d347d6e89ced072c4272e
Virgilio Gómez-Rubio <virgilio.gomez@uclm.es>
2021-12-14 22:00:11 +0100

    Script to install all required packages.

---
+++ 00_install_pakgs.R
@@ -0,0 +1,13 @@
+# Run this script first in order to install all the required packages
+
+
+pkgs <- c("dplyr", "ecmwfr", "FNN", "ggplot2", "grid", "lctools", "lubridate", 
+  "maptools", "ncdf4", "patchwork", "pbapply", "plotly", "plyr", 
+  "raster", "RColorBrewer", "readr", "reshape2", "rgdal", "sf", 
+  "sp", "spacetime", "spdep", "stringr", "tidyr", "tidyverse", 
+  "timeDate", "viridis", "xtable", "xts")
+install.packages(pkgs, dep = TRUE)
+
+# Install INLA (off CRAN)
+install.packages("INLA", repos = c(getOption("repos"), 
+  INLA = "https://inla.r-inla-download.org/R/stable"), dep = TRUE)
