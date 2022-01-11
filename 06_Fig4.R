2ff3fba077841f2eef86ba3f7b9da73f3a97d7d3
Garyfallos Konstantinoudis <garyfallos.konstantinoudis@ispm.unibe.ch>
2022-01-11 14:34:39 +0000

    Change name of figures

--- 06_Fig4.R
+++ 06_Fig4.R
@@ -1,248 +1,67 @@
 
 
-# Created 19.11.2021
+# Created 10.12.2021 
 
 
-# Fig. 4
+# Fig 3. PC prior plots
 
 
 #---------------------------------------------------------------------------------
 
-library(dplyr)
+library(INLA)
 library(ggplot2)
-library(sf)
-library(viridis)
-library(lubridate)
-library(ISOweek)
 library(patchwork)
 library(sf)
-library(RColorBrewer)
-library(leaflet)
+library(spdep)
 
 
-load("Output/Italy.RData")
+# prior for standard deviation
 
-# get the shp
-shp <- read_sf("data/ProvCM01012020_g_WGS84.shp")
-link_table <- readRDS("data/link_table")
+xvals = seq(0.1, 10, length.out=1000)
+lambda=-log(0.01)/1
 
-# and for NUTS2
-shp %>% left_join(., link_table, by = c("SIGLA" = "SIGLA")) %>% 
-  group_by(NAMNUTS2) %>% 
-  summarise() -> shp_NUTS2
-  
-
-# get the iso correspondance between months for the xaxis
-
-date.iso <- data.frame(
-  iso = rep(d_week$country$sex$F$EURO_LABEL, each = 7),
-  date = ISOweek2date(unlist(lapply(d_week$country$sex$F$EURO_LABEL, function(X) paste(X, 1:7, sep = "-"))))
-)
-
-date.iso$month <- month(date.iso$date)
-date.iso$month_nam <- month.abb[date.iso$month]
-
-d_week$country$sex$M %>% 
-  left_join(., date.iso, by = c("EURO_LABEL" = "iso")) %>% 
-  mutate(x = as.numeric(as.factor(EURO_LABEL))) -> b
-
-b <-  b[, c("month_nam","x")]
-b <- b[-c(1:2),]
-
-b <- b[!duplicated(b$month_nam),]
+ggplot() + geom_line(aes(x=xvals, y=lambda*exp(-lambda*xvals))) + 
+  theme_light() + ylab(expression("Implied prior for "~sigma[b])) + 
+  xlab(expression(sigma[b])) + ggtitle("A.") + 
+  theme(text = element_text(size = 6)) -> p1
 
 
 
-colors <- rev(brewer.pal(n = 10, name = "RdBu")[1:6])
-col.highlight <- "blue"
+# prior for mixing parameter
 
-# First pannel
-ggplot(d$country$none) + geom_sf(data = d$country$none, aes(fill = median.REM.cat), size = 0.4) + 
-  geom_sf(fill = NA, col = col.highlight, size = 0.6) + 
-  scale_fill_manual(values=colors, name = "", drop=FALSE) + theme_light() +
-  ggtitle("1A. National") + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> L1
+shp = read_sf("data/ProvCM01012020_g_WGS84.shp")
+W.nb <- poly2nb(shp)
+nb2INLA("W.adj", W.nb) 
 
 
+# Create the precision matrix
+pol <- spdep::poly2nb(shp)
+W <- spdep::nb2mat(pol, style = "B", zero.policy = TRUE)
+W <- -W
+diag(W) <- abs(apply(W, 1, sum))
+n <- nrow(W)
+QQ <- W
 
-d_week$country$sex$M %>% 
-  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
-  
-  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
-  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
-  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
-  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
-  theme_light() + ylim(c(-50, 170)) + ggtitle("1B. Males") + ylab("") + xlab("") + 
-  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> P11
+
+log.prior = INLA:::inla.pc.bym.phi(Q = QQ, rankdef = 1, u = 0.5, alpha = 0.5)
+phis = 1/(1+exp(-seq(-7, 7, len=10000)))
+
+ggplot() + geom_line(aes(x=phis, y=exp(log.prior(phis)))) + 
+  theme_light() + ylab(expression("PC prior for "~phi)) + 
+  xlab(expression(phi)) + ggtitle("B.") + 
+  theme(text = element_text(size = 6)) -> p2
 
 
 
-d_week$country$sex$F %>% mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
-  
-  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
-  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
-  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
-  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
-  theme_light() + ylim(c(-50, 170)) + ggtitle("1C. Females") + ylab("") + xlab("") + 
-  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> P12
-
-# P1/P2
-
-L1|(P11/P12) -> M1
-
-
-# Second pannel
-
-region <- d$region$none %>% filter(median.REM == max(median.REM)) %>% select(NAMNUTS2)
-region$geometry <- NULL
-region <- "Puglia"
-
-
-
-
-d$region$none %>% filter(NAMNUTS2 %in% region) %>% 
-  select(geometry) %>% 
-ggplot() +  
-  geom_sf(data = d$region$none, aes(fill = median.REM.cat), size = 0.4) + 
-  geom_sf(fill = NA, col = col.highlight, size = 0.6) + 
-  scale_fill_manual(values=colors, name = "", drop=FALSE) + theme_light() + 
-  ggtitle(paste0("2A. NUTS2 regions: ", region)) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> L2
-
-
-d_week$region$sex$M %>% filter(NAMNUTS2 %in% region) %>% 
-  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
-  
-  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
-  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
-  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
-  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
-  theme_light() + ylim(c(-50, 170)) + ggtitle("2B. Males") + ylab("") + xlab("") + 
-  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> P21
-
-
-
-d_week$region$sex$F %>% filter(NAMNUTS2 %in% region) %>% 
-  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
-  
-  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
-  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
-  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
-  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
-  theme_light() + ylim(c(-50, 170)) + ggtitle("2C. Females") + ylab("") + xlab("") + 
-  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> P22
-
-
-L2|(P21/P22) -> M2
-
-
-
-
-
-
-
-
-# Third pannel
-
-prov <- d$province$none %>% filter(median.REM == max(median.REM)) %>% select(ID_space)
-prov$geometry <- NULL
-prov <- "Foggia"
-
-
-d$province$none %>% filter(DEN_UTS == prov) %>% 
-  select(geometry) %>% 
-  ggplot() +  
-  geom_sf(data = d$province$none, aes(fill = median.REM.cat), size = 0.4) + 
-  geom_sf(fill = NA, col = col.highlight, size = .6) + 
-  scale_fill_manual(values=colors, name = "", drop=FALSE) + theme_light() +
-  ggtitle(paste0("3A. NUTS3 regions: ", prov)) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> L3
-
-
-
-prov <- d$province$none %>% filter(DEN_UTS == prov) %>% select(ID_space)
-prov$geometry <- NULL
-
-d_week$province$sex$M %>% filter(ID_space %in% prov) %>% 
-  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
-  
-  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
-  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
-  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
-  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
-  theme_light() + ylim(c(-50, 170)) + ggtitle("3B. Males") + ylab("") + xlab("") + 
-  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> P31
-
-
-
-d_week$province$sex$F %>% filter(ID_space %in% prov) %>% 
-  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
-  
-  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
-  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
-  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
-  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
-  theme_light() + ylim(c(-50, 170)) + ggtitle("3C. Females") + ylab("") + xlab("") + 
-  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
-  theme(text = element_text(size = 6),  
-        legend.key.height = unit(0.3, 'cm'),
-        legend.key.width = unit(0.2, 'cm'), 
-        plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> P32
-
-
-L3|(P31/P32) -> M3
-
-png("Output/SpatiotemporalRegions.png", width = 16.5, height = 22, res = 300, units = "cm")
-print(
-M1/M2/M3
-)
+png("Output/PCpriors.png", width = 14, height = 5, units = "cm", res = 300)
+(p1|p2) 
 dev.off()
 
 
 
-
-####################################################################
-####################################################################
-####################################################################
-####################################################################
-####################################################################
+######################################################################################
+######################################################################################
+######################################################################################
+######################################################################################
 
 
--- 06_Fig5.R
+++ 06_Fig5.R
@@ -3,76 +3,246 @@
 # Created 19.11.2021
 
 
-# Fig. 5
+# Fig. 4
 
 
 #---------------------------------------------------------------------------------
-
 
 library(dplyr)
 library(ggplot2)
 library(sf)
 library(viridis)
+library(lubridate)
+library(ISOweek)
 library(patchwork)
+library(sf)
 library(RColorBrewer)
+library(leaflet)
 
 
 load("Output/Italy.RData")
 
-cols_exp <- brewer.pal(n = 11, name = "RdBu")
-cols_exp <- cols_exp[c(2, 4, 6, 8, 10)]
+# get the shp
+shp <- read_sf("data/ProvCM01012020_g_WGS84.shp")
+link_table <- readRDS("data/link_table")
 
+# and for NUTS2
+shp %>% left_join(., link_table, by = c("SIGLA" = "SIGLA")) %>% 
+  group_by(NAMNUTS2) %>% 
+  summarise() -> shp_NUTS2
+  
+
+# get the iso correspondance between months for the xaxis
+
+date.iso <- data.frame(
+  iso = rep(d_week$country$sex$F$EURO_LABEL, each = 7),
+  date = ISOweek2date(unlist(lapply(d_week$country$sex$F$EURO_LABEL, function(X) paste(X, 1:7, sep = "-"))))
+)
+
+date.iso$month <- month(date.iso$date)
+date.iso$month_nam <- month.abb[date.iso$month]
+
+d_week$country$sex$M %>% 
+  left_join(., date.iso, by = c("EURO_LABEL" = "iso")) %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL))) -> b
+
+b <-  b[, c("month_nam","x")]
+b <- b[-c(1:2),]
+
+b <- b[!duplicated(b$month_nam),]
+
+
+
+colors <- rev(brewer.pal(n = 10, name = "RdBu")[1:6])
+col.highlight <- "blue"
 
 # First pannel
-ggplot() + geom_sf(data = d$province$age$`40<`, aes(fill = exceedance.REM.cat), size = 0.4) + 
-  scale_fill_manual(values=cols_exp[length(cols_exp):1], name = "", drop=FALSE) + theme_light() +
-  ggtitle("A. 40<") + 
-  theme(text = element_text(size = 6), 
-        legend.position = 'none', 
-        legend.margin=margin(0,0,0,0)) -> L1
-
-
-ggplot() + geom_sf(data = d$province$age$`40-59`, aes(fill = exceedance.REM.cat), size = 0.4) + 
-  scale_fill_manual(values=cols_exp[length(cols_exp):1], name = "", drop=FALSE) + theme_light() +
-  ggtitle("B. 40-59") + 
-  theme(text = element_text(size = 6), 
-        legend.position = 'none', 
-        legend.margin=margin(0,0,0,0)) -> L2
-
-
-ggplot() + geom_sf(data = d$province$age$`60-69`, aes(fill = exceedance.REM.cat), size = 0.4) + 
-  scale_fill_manual(values=cols_exp[length(cols_exp):1], name = "", drop=FALSE) + theme_light() +
-  ggtitle("C. 60-69") + 
-  theme(text = element_text(size = 6), 
-        legend.position = 'none', 
-        legend.margin=margin(0,0,0,0)) -> L3
-
-ggplot() + geom_sf(data = d$province$age$`70-79`, aes(fill = exceedance.REM.cat), size = 0.4) + 
-  scale_fill_manual(values=cols_exp[length(cols_exp):1], name = "", drop=FALSE) + theme_light() +
-  ggtitle("D. 70-79") + 
-  theme(text = element_text(size = 6), 
-        legend.position = 'none', 
-        legend.margin=margin(0,0,0,0)) -> L4
-
-ggplot() + geom_sf(data = d$province$age$`80+`, aes(fill = exceedance.REM.cat), size = 0.4) + 
-  scale_fill_manual(values=cols_exp[length(cols_exp):1], name = "", drop=FALSE) + theme_light() +
-  ggtitle("E. 80>") + 
+ggplot(d$country$none) + geom_sf(data = d$country$none, aes(fill = median.REM.cat), size = 0.4) + 
+  geom_sf(fill = NA, col = col.highlight, size = 0.6) + 
+  scale_fill_manual(values=colors, name = "", drop=FALSE) + theme_light() +
+  ggtitle("1A. National") + 
   theme(text = element_text(size = 6),  
         legend.key.height = unit(0.3, 'cm'),
         legend.key.width = unit(0.2, 'cm'), 
         plot.margin = margin(0, 0, 0, 0), 
-        legend.margin=margin(0,0,0,0)) -> L5
+        legend.margin=margin(0,0,0,0)) -> L1
 
 
-png("Output/PosteriorProb.png", width = 17, height = 14, res = 300, units = "cm")
+
+d_week$country$sex$M %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
+  
+  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
+  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
+  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
+  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
+  theme_light() + ylim(c(-50, 170)) + ggtitle("1B. Males") + ylab("") + xlab("") + 
+  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> P11
+
+
+
+d_week$country$sex$F %>% mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
+  
+  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
+  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
+  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
+  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
+  theme_light() + ylim(c(-50, 170)) + ggtitle("1C. Females") + ylab("") + xlab("") + 
+  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> P12
+
+# P1/P2
+
+L1|(P11/P12) -> M1
+
+
+# Second pannel
+
+region <- d$region$none %>% filter(median.REM == max(median.REM)) %>% select(NAMNUTS2)
+region$geometry <- NULL
+region <- "Puglia"
+
+
+
+
+d$region$none %>% filter(NAMNUTS2 %in% region) %>% 
+  select(geometry) %>% 
+ggplot() +  
+  geom_sf(data = d$region$none, aes(fill = median.REM.cat), size = 0.4) + 
+  geom_sf(fill = NA, col = col.highlight, size = 0.6) + 
+  scale_fill_manual(values=colors, name = "", drop=FALSE) + theme_light() + 
+  ggtitle(paste0("2A. NUTS2 regions: ", region)) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> L2
+
+
+d_week$region$sex$M %>% filter(NAMNUTS2 %in% region) %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
+  
+  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
+  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
+  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
+  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
+  theme_light() + ylim(c(-50, 170)) + ggtitle("2B. Males") + ylab("") + xlab("") + 
+  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> P21
+
+
+
+d_week$region$sex$F %>% filter(NAMNUTS2 %in% region) %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
+  
+  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
+  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
+  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
+  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
+  theme_light() + ylim(c(-50, 170)) + ggtitle("2C. Females") + ylab("") + xlab("") + 
+  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> P22
+
+
+L2|(P21/P22) -> M2
+
+
+
+
+
+
+
+
+# Third pannel
+
+prov <- d$province$none %>% filter(median.REM == max(median.REM)) %>% select(ID_space)
+prov$geometry <- NULL
+prov <- "Foggia"
+
+
+d$province$none %>% filter(DEN_UTS == prov) %>% 
+  select(geometry) %>% 
+  ggplot() +  
+  geom_sf(data = d$province$none, aes(fill = median.REM.cat), size = 0.4) + 
+  geom_sf(fill = NA, col = col.highlight, size = .6) + 
+  scale_fill_manual(values=colors, name = "", drop=FALSE) + theme_light() +
+  ggtitle(paste0("3A. NUTS3 regions: ", prov)) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> L3
+
+
+
+prov <- d$province$none %>% filter(DEN_UTS == prov) %>% select(ID_space)
+prov$geometry <- NULL
+
+d_week$province$sex$M %>% filter(ID_space %in% prov) %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
+  
+  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
+  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
+  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
+  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
+  theme_light() + ylim(c(-50, 170)) + ggtitle("3B. Males") + ylab("") + xlab("") + 
+  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> P31
+
+
+
+d_week$province$sex$F %>% filter(ID_space %in% prov) %>% 
+  mutate(x = as.numeric(as.factor(EURO_LABEL))) %>% 
+  
+  ggplot() + geom_line(aes(x = x, y = median.REM)) + 
+  geom_point(aes(x = x, y = median.REM), size = 0.8) + 
+  geom_ribbon(aes(x = x, ymin = LL.REM, ymax = UL.REM), alpha = 0.3, fill = viridis(15)[6]) +
+  geom_hline(yintercept = 0, col = "red", linetype = 2) + 
+  theme_light() + ylim(c(-50, 170)) + ggtitle("3C. Females") + ylab("") + xlab("") + 
+  scale_x_continuous(minor_breaks = NULL, breaks = b$x, labels = b$month_nam) + 
+  theme(text = element_text(size = 6),  
+        legend.key.height = unit(0.3, 'cm'),
+        legend.key.width = unit(0.2, 'cm'), 
+        plot.margin = margin(0, 0, 0, 0), 
+        legend.margin=margin(0,0,0,0)) -> P32
+
+
+L3|(P31/P32) -> M3
+
+png("Output/SpatiotemporalRegions.png", width = 16.5, height = 22, res = 300, units = "cm")
 print(
-(L1|L2|L3)/(L4|L5)
+M1/M2/M3
 )
 dev.off()
 
 
+
+
 ####################################################################
 ####################################################################
 ####################################################################
 ####################################################################
+####################################################################
+
 
